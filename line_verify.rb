c90ebe73e7fcaca4388e0ab870cbc442328a3655
xiang940804 <168198823+xiang940804@users.noreply.github.com>
2024-04-26 21:47:05 +0800

    Add files via upload

---
+++ line_verify.rb
@@ -0,0 +1,38 @@
+{
+    "events" => [
+        {
+            "replyToken" => "00000000000000000000000000000000",
+            "type"=>"message",
+            "timestamp"=>1515423877419,
+            "source"=>{
+                "type"=>"user",
+                "userId"=>"Udeadbeefdeadbeefdeadbeefdeadbeef"
+            },
+            "message"=>{
+                "id"=>"100001",
+                "type"=>"text",
+                "text"=>"Hello, world"
+            }
+        },
+        {
+            "replyToken" => "ffffffffffffffffffffffffffffffff",
+            "type"=>"message",
+            "timestamp"=>1515423877419,
+            "source"=>{
+                "type"=>"user",
+                "userId"=>"Udeadbeefdeadbeefdeadbeefdeadbeef"
+            },
+            "message"=>{
+                "id"=>"100002",
+                "type"=>"sticker",
+                "packageId"=>"1",
+                "stickerId"=>"1"
+            }
+        }
+    ],
+    "kamigo" => {
+        "events" => [
+
+        ]
+    }
+}
